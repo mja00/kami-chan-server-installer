e58ea2a7effa9874b3cfe0ec1bbdbb951d8e4bbb
mja00 <me@mja00.dev>
2024-08-26 06:38:09 -0400

    feat: nix?

---
+++ nix/default.nix
@@ -0,0 +1,29 @@
+let
+  # Import nixpkgs if needed
+  pkgs = import <nixpkgs> {};
+in
+  {
+    lib ? pkgs.lib,
+    buildGoModule ? pkgs.buildGoModule,
+    fetchFromGitHub ? pkgs.fetchFromGitHub,
+    installShellFiles ? pkgs.installShellFiles,
+    # version and vendorSha256 should be specified by the caller
+    version ? "latest",
+    vendorSha256,
+  }:
+    buildGoModule rec {
+      pname = "kami-chan-server-installer";
+      inherit version vendorSha256;
+
+      src = ./..;
+
+      nativeBuildInputs = [
+        installShellFiles
+      ];
+
+      meta = with lib; {
+        description = "A command line tool installing Paper servers";
+        license = licenses.mit;
+        mainProgram = "kami-chan-server-installer";
+      };
+    }
