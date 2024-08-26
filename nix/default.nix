e06070d010b453b75fa6ef08aca0e0b8f4261185
mja00 <me@mja00.dev>
2024-08-26 07:01:58 -0400

    fix: forgotten var

--- nix/default.nix
+++ nix/default.nix
@@ -7,13 +7,13 @@ in
     buildGoModule ? pkgs.buildGoModule,
     fetchFromGitHub ? pkgs.fetchFromGitHub,
     installShellFiles ? pkgs.installShellFiles,
-    # version and vendorSha256 should be specified by the caller
+    # version and vendorHash should be specified by the caller
     version ? "latest",
     vendorHash,
   }:
     buildGoModule rec {
       pname = "kami-chan-server-installer";
-      inherit version vendorSha256;
+      inherit version vendorHash;
 
       src = ./..;
 
